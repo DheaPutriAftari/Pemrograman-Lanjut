e59001a72d7500761dc4ebd747655e9fe29ddf74
DheaPutriAftari <80236915+DheaPutriAftari@users.noreply.github.com>
2021-04-17 10:56:43 +0700

    Add files via upload
    Kodingan mencari data mahasiswa berdasarkan NIM

---
+++ src/Tugas Mencari Data Mahasiwa Berdasarkan NIM/NIMRun.java
@@ -0,0 +1,32 @@
+package TugasNim;
+
+import java.util.Scanner;
+
+public class NIMRun {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        System.out.println("Nama Mahasiswa     :"); 
+        String nama = input.nextLine();
+        
+        System.out.println("NIM                :"); 
+        String nim = input.nextLine();
+        NIM Nim = new NIM(); 
+        Nim.setNim(nim); 
+    
+    
+    
+   System.out.println("Jenjang Pendidikan :" + Nim.getJenjangPendidikan()); 
+    
+   System.out.println("Tahun masuk        :" + Nim.getTahunMasuk()); 
+    
+   System.out.println("Fakultas           :" + Nim.getFakultas());
+    
+   System.out.println("Jurusan            :" + Nim.getJurusan());
+    
+   System.out.println("Jenis Kelamin      :" + Nim.getJenisKelamin()); 
+    
+   System.out.println("Nomor Urut         :" + Nim.getNomorUrut()); 
+    
+    }  
+}
